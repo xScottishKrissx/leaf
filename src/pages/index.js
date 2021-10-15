b4b3c1e9b3073b2ba2ff02148fcff08042dbf812
Chris Dunne <chrisdunne66@gmail.com>
2021-10-15 18:29:56 +0100

    0.4 - Working on scroll to load

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { useState, useEffect, useRef } from "react"
+
 import './index.sass'
 
 import { Link, graphql } from "gatsby"
@@ -11,7 +13,172 @@ import leaf from '../images/leaf.png'
 import placeholderPlant from '../images/placeholderPlant.jpg'
 import { Button, Container,Row, Col } from 'react-bootstrap';
 
+// export default class BlogIndex extends React.Component {
 
+//   constructor(props){
+//     super(props);
+//     this.state = {
+        
+//     }
+// }
+
+//   showPopup(id){
+//     console.log("Show Popup " + id)
+//     document.getElementById("popup").classList.add("showWrapper")
+//   }
+//   hidePopup(){
+//     console.log("Hide Popup")
+//     document.getElementById("popup").classList.remove("showWrapper")
+//   }
+
+//   intersectionCallback(entries) {
+//     console.log(entries)
+//     entries.forEach(function(entry) {
+     
+//       if (entry.isIntersecting) {
+//         let targetRow = entry.target.className
+        
+//         if(targetRow.includes( "itemWrapper")){
+          
+//           console.log("Load More articles")
+
+//         }
+//       }else{}
+//     });
+
+//   }   
+  
+
+
+//   render(){
+
+//     let options = { threshold: [0.1] }
+//     let observer = new IntersectionObserver(this.intersectionCallback, options)
+//     let elements = document.querySelectorAll('div')
+    
+//     for (let elm of elements){ 
+//       observer.observe(elm) 
+//     }
+
+//     const siteTitle = this.props.data.site.siteMetadata?.title || `Title`
+//     const posts = this.props.data.allMarkdownRemark.nodes
+//     console.log(posts)
+
+//     return (
+//       <>
+//       <Container fluid >
+  
+  
+//         <Row className="header"> 
+//           <Col >
+//             <div className="section1">
+//               <span className="heroImage"></span>
+//               <div>
+//                 <span>welcome to</span>
+//                 <h1>leaf.com </h1>
+//                 <button >Increase Hook State thing</button>
+//                 <img src={leaf} />
+//               </div>
+  
+//             </div>
+//           </Col>
+//         </Row>
+  
+  
+  
+//       </Container>
+  
+//       <Container className="bodyContent" fluid="lg">
+//         <Row className="bodySection1">
+  
+//           <Col lg={12}>
+//             <div>
+//               <h1>Plant Gallery</h1>
+//               <Row className="itemWrapper" >
+//                 <Col md={3} sm={6} onClick={()=>this.showPopup(1)}>
+//                   <img src={placeholderPlant} />
+//                   <h3>Title</h3>
+//                   <span>laternus nameinus</span>
+//                 </Col>
+  
+//                 <Col md={3} sm={6} onClick={()=>this.showPopup(2)}>
+//                   <img src={placeholderPlant} />
+//                   <h3>Title</h3>
+//                   <span>laternus nameinus</span>
+//                 </Col>
+  
+//                 <Col md={3} sm={6} onClick={()=>this.showPopup(3)}>
+//                   <img src={placeholderPlant} />
+//                   <h3>Title</h3>
+//                   <span>laternus nameinus</span>
+//                 </Col>
+  
+//                 <Col md={3} sm={6}>
+//                   <img src={placeholderPlant} />
+//                   <h3>Title</h3>
+//                   <span>laternus nameinus</span>
+//                 </Col>
+  
+//                 <Col md={3} sm={6}>
+//                   <img src={placeholderPlant} />
+//                   <h3>Title</h3>
+//                   <span>laternus nameinus</span>
+//                 </Col>
+  
+//                 <Col md={3} sm={6}>
+//                   <img src={placeholderPlant} />
+//                   <h3>Title</h3>
+//                   <span>laternus nameinus</span>
+//                 </Col>
+  
+//                 <Col md={3} sm={6}>
+//                   <img src={placeholderPlant} />
+//                   <h3>Title</h3>
+//                   <span>laternus nameinus</span>
+//                 </Col>
+  
+//                 <Col md={3} sm={6} >
+//                   <img src={placeholderPlant} />
+//                   <h3>Title</h3>
+//                   <span>laternus nameinus</span>
+//                 </Col>
+//               </Row>
+//             </div>
+//           </Col>
+  
+//           <div className="popupWrapper" id="popup" >
+            
+//             <div className="popupContent" onClick={null}>
+  
+//               <div className="popupColumn1">
+//                 <h1>Image</h1>
+//               </div>
+//               <div className="popupColumn2">
+//                 <h1>Title</h1>
+//                 <h2>Text</h2>
+//               </div>
+  
+//             </div>
+//             <div onClick={()=>this.hidePopup()} className="popupBackground" ></div>
+//           </div>
+  
+//         </Row>
+  
+//         <Row className="bodySection2">
+//           <Col lg={12}>
+//             <div>
+//               <h1>Body Section 2</h1>
+//             </div>
+//           </Col>
+//         </Row>
+  
+//       </Container>
+  
+//       </>
+//     )
+//   }
+
+// }
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -28,7 +195,41 @@ const BlogIndex = ({ data, location }) => {
     document.getElementById("popup").classList.remove("showWrapper")
   }
 
+  const [count, setCount] = useState(0)
 
+  useEffect(()=>{
+    
+    function intersectionCallback(entries) {
+
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let targetRow = entry.target.className
+          if(targetRow.includes( "itemWrapper")){
+            // console.log(targetRow)
+            console.log("Load More articles")
+            setCount(count + 1)
+          }
+        }else{}
+      });
+  
+    }   
+    
+    let options = { threshold: [0.1] }
+    let observer = new IntersectionObserver(intersectionCallback, options)
+    let elements = document.querySelectorAll('div')
+
+    let elm
+    for (elm of elements){ 
+      observer.observe(elm) 
+      
+    }
+
+
+}, [])
+
+
+
+  
   return (
     <>
     <Container fluid >
@@ -40,7 +241,8 @@ const BlogIndex = ({ data, location }) => {
             <span className="heroImage"></span>
             <div>
               <span>welcome to</span>
-              <h1>leaf.com</h1>
+              <h1>leaf.com {count}</h1>
+              <button >Increase Hook State thing</button>
               <img src={leaf} />
             </div>
 
@@ -52,7 +254,7 @@ const BlogIndex = ({ data, location }) => {
 
     </Container>
 
-    <Container className="bodyContent" fluid="lg">
+    <Container className="bodyContent" fluid="lg" >
       <Row className="bodySection1">
 
         <Col lg={12}>
@@ -101,7 +303,7 @@ const BlogIndex = ({ data, location }) => {
                 <span>laternus nameinus</span>
               </Col>
 
-              <Col md={3} sm={6}>
+              <Col md={3} sm={6} >
                 <img src={placeholderPlant} />
                 <h3>Title</h3>
                 <span>laternus nameinus</span>
@@ -125,7 +327,7 @@ const BlogIndex = ({ data, location }) => {
           </div>
           <div onClick={()=>hidePopup()} className="popupBackground" ></div>
         </div>
-        
+
       </Row>
 
       <Row className="bodySection2">
