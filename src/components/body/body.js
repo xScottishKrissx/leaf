9637e3283a5ffcb30913c87aa86f248bf527ab7b
Chris Dunne <chrisdunne66@gmail.com>
2021-10-21 18:20:03 +0100

    0.7.3 - Gallery In

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -17,59 +17,16 @@ import BlogGallery from './blogGallery/blogGallery'
 
 export const Body = (props) => {
 
-    const mapGallery = props.posts.map(obj => {
-        return(
-  
-  
-        <Col xl={4} md={6} sm={6} onClick={()=>this.showPopup(obj.frontmatter.title,obj.frontmatter.title)}>
-          <div>
-            <img src={placeholderPlant} />
-            <div>
-              <h3>{obj.frontmatter.title}</h3>
-              <span>laternus nameinus</span>
-            </div>
-          </div>
-  
-  
-        </Col>
-        )
-    })
+
 
     return (
         <div>
                   <Container className="bodyContent" fluid="lg">
-                    <Gallery />
+                    <Gallery posts={props.posts}/>
                     <Bio />
                     <Uses />
                     <BlogGallery />
-        <Row className="bodySection1">
-  
-          <Col lg={12}>
-            <div>
-              <h1>Plant Gallery</h1>
-              <Row className="itemWrapper" > {mapGallery} </Row>
-            </div>
-          </Col>
 
-          
-  
-          {/* <div className="popupWrapper" id="popup" >
-            
-            <div className="popupContent" onClick={null}>
-  
-              <div className="popupColumn1">
-                <h1>Image</h1>
-              </div>
-              <div className="popupColumn2">
-                <h1>Title</h1>
-                <h2>Text</h2>
-              </div>
-  
-            </div>
-            <div onClick={()=>this.hidePopup()} className="popupBackground" ></div>
-          </div>
-   */}
-        </Row>
   
         <Row className="bodySection2">
 
--- src/components/body/gallery/gallery.js
+++ src/components/body/gallery/gallery.js
@@ -2,12 +2,59 @@ import * as React from 'react'
 
 import './gallery.sass'
 
+import { Container, Row, Col, Button } from 'react-bootstrap'
+
+import placeholderPlant from '../../../images/placeholderPlant.jpg'
+
 export const Gallery = (props) =>{
+
+    const mapGallery = props.posts.map(obj => {
+        return(
+  
+  
+        <Col xl={4} md={6} sm={6} onClick={()=>this.showPopup(obj.frontmatter.title,obj.frontmatter.title)}>
+          <div>
+            <img src={placeholderPlant} />
+            <div>
+              <h3>{obj.frontmatter.title}</h3>
+              <span>laternus nameinus</span>
+            </div>
+          </div>
+  
+  
+        </Col>
+        )
+    })
     
 return(
-    <div>
-        <h1>Gallery</h1>
-    </div>
+    <Row className="bodySection1">
+  
+    <Col lg={12}>
+      <div>
+        <h1>Plant Gallery</h1>
+        <Row className="itemWrapper" > {mapGallery} </Row>
+      </div>
+    </Col>
+
+    
+
+        {/* <div className="popupWrapper" id="popup" >
+        
+        <div className="popupContent" onClick={null}>
+
+            <div className="popupColumn1">
+            <h1>Image</h1>
+            </div>
+            <div className="popupColumn2">
+            <h1>Title</h1>
+            <h2>Text</h2>
+            </div>
+
+        </div>
+        <div onClick={()=>this.hidePopup()} className="popupBackground" ></div>
+        </div>
+    */}
+  </Row>
 )
 
 }
