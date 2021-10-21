53af41623610d7235ec44b81e9178d3480a7d898
Chris Dunne <chrisdunne66@gmail.com>
2021-10-21 18:17:41 +0100

    0.7.2 - Body In

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,19 +1,184 @@
 import * as React from 'react'
+
+import './body.sass'
+
+import { Container, Row, Col, Button } from 'react-bootstrap'
+
+import placeholderPlant from '../../images/placeholderPlant.jpg'
+import who1 from '../../images/who_1.png'
+import who2 from '../../images/who_2.png'
+import who3 from '../../images/who_3.png'
+
+
+import Gallery from './gallery/gallery'
+import Uses from './uses/uses'
 import Bio from './bio/bio'
 import BlogGallery from './blogGallery/blogGallery'
 
-import './body.sass'
-import Gallery from './gallery/gallery'
-import Uses from './uses/uses'
-
 export const Body = (props) => {
+
+    const mapGallery = props.posts.map(obj => {
+        return(
+  
+  
+        <Col xl={4} md={6} sm={6} onClick={()=>this.showPopup(obj.frontmatter.title,obj.frontmatter.title)}>
+          <div>
+            <img src={placeholderPlant} />
+            <div>
+              <h3>{obj.frontmatter.title}</h3>
+              <span>laternus nameinus</span>
+            </div>
+          </div>
+  
+  
+        </Col>
+        )
+    })
+
     return (
         <div>
-            <h1>Body</h1>
-            <Gallery />
-            <Bio />
-            <Uses />
-            <BlogGallery />
+                  <Container className="bodyContent" fluid="lg">
+                    <Gallery />
+                    <Bio />
+                    <Uses />
+                    <BlogGallery />
+        <Row className="bodySection1">
+  
+          <Col lg={12}>
+            <div>
+              <h1>Plant Gallery</h1>
+              <Row className="itemWrapper" > {mapGallery} </Row>
+            </div>
+          </Col>
+
+          
+  
+          {/* <div className="popupWrapper" id="popup" >
+            
+            <div className="popupContent" onClick={null}>
+  
+              <div className="popupColumn1">
+                <h1>Image</h1>
+              </div>
+              <div className="popupColumn2">
+                <h1>Title</h1>
+                <h2>Text</h2>
+              </div>
+  
+            </div>
+            <div onClick={()=>this.hidePopup()} className="popupBackground" ></div>
+          </div>
+   */}
+        </Row>
+  
+        <Row className="bodySection2">
+
+          <Col lg={12}>
+            <h1>Who are Leaf.com?</h1>
+              <Row className="sectionContentWrapper m-0">
+                <Col>
+                  <img src={who1} />
+                  <p>Database</p>
+                </Col>
+
+                <Col>
+                  <img src={who2} />
+                  <p>Preservation</p>
+                </Col>
+
+                <Col>
+                  <img src={who3} />
+                  <p>Knowledge</p>
+                </Col>
+              </Row>
+          </Col>
+        </Row>
+
+
+        <Row className="bodySection3">
+
+          <Col lg={12}>
+            <h1>Plant Uses</h1>
+              <Row className="sectionContentWrapper upper m-0 mb-3 ">
+
+                <Col id="forcePaddingRight" md={6} className="d-flex justify-content-center justify-content-md-end">
+                  <img src={placeholderPlant} />
+                </Col>
+
+                <Col md={6} className="d-flex justify-content-center align-items-center">
+                  <div className="p-5">
+                    <h2>Edible</h2>
+                    <p>Plants bring homes to life, and they also bring life to your office. Adding plants into your space also helps to create a relaxing environment.</p>
+                    <p>Plants bring homes to life, and they also bring life to your office. Adding plants into your space also helps to create a relaxing environment.</p>
+                    <Button variant="success">Success</Button>
+                  </div>
+                </Col>
+              
+
+              </Row>
+
+              <Row className="sectionContentWrapper m-0 lower">
+                <Col id="forcePaddingLeft" md={6} className="d-flex justify-content-center justify-content-md-start">
+                  <img src={placeholderPlant} />
+                </Col>
+        
+                <Col md={6} className="d-flex justify-content-center align-items-center">
+                  <div className="p-5">
+                    <h2>Medicinal</h2>
+                    <p>We've gathered some of our most popular plants into sets to suit all types of plant parent, including those who need a bit of extra inspiration.</p>
+                    <p>Find your match</p>
+                    <Button variant="success">Success</Button>
+                  </div>
+                </Col>
+
+              </Row>
+          </Col>
+
+        </Row>
+
+        <Row className="bodySection4">
+
+          <Col lg={12}>
+            <h1>Blog Pages</h1>
+              <Row className="sectionContentWrapper">
+                <Col sm={6} lg={3}>
+                  <div className="pb-3">
+                    <img src={placeholderPlant} />
+                    <p>Blog Title</p>
+                    <p>Blog Copy</p>
+                  </div>
+                </Col>
+
+                <Col sm={6} lg={3}>
+                  <div>
+                    <img src={placeholderPlant} />
+                    <p>Blog Title</p>
+                    <p>Blog Copy</p>
+                  </div>
+                </Col>
+
+                <Col sm={6} lg={3}>
+                  <div>
+                    <img src={placeholderPlant} />
+                    <p>Blog Title</p>
+                    <p>Blog Copy</p>
+                  </div>
+                </Col>
+
+                <Col sm={6} lg={3}>
+                  <div>
+                    <img src={placeholderPlant} />
+                    <p>Blog Title</p>
+                    <p>Blog Copy</p>
+                  </div>
+                </Col>
+              </Row>
+          </Col>
+
+          </Row>
+  
+      </Container>
+
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,7 @@ import Seo from "../components/seo"
 
 import leaf from '../images/leaf.png'
 
-import placeholderPlant from '../images/placeholderPlant.jpg'
-import who1 from '../images/who_1.png'
-import who2 from '../images/who_2.png'
-import who3 from '../images/who_3.png'
+
 
 import { Button, Container,Row, Col, Stack } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal'
@@ -71,170 +68,18 @@ export default class BlogIndex extends React.Component {
     const posts = this.state.galleryArray
     console.log(posts)
 
-    const mapGallery = posts.map(obj => {
-      return(
 
-
-      <Col xl={4} md={6} sm={6} onClick={()=>this.showPopup(obj.frontmatter.title,obj.frontmatter.title)}>
-        <div>
-          <img src={placeholderPlant} />
-          <div>
-            <h3>{obj.frontmatter.title}</h3>
-            <span>laternus nameinus</span>
-          </div>
-        </div>
-
-
-      </Col>
-      )
-    })
 
     return (
       <>
       <Header />
-      <Body />
+      <Body posts={posts} />
       <Footer />
       
 
 
 
-      <Container className="bodyContent" fluid="lg">
-        <Row className="bodySection1">
-  
-          <Col lg={12}>
-            <div>
-              <h1>Plant Gallery</h1>
-              <Row className="itemWrapper" > {mapGallery} </Row>
-            </div>
-          </Col>
 
-          
-  
-          {/* <div className="popupWrapper" id="popup" >
-            
-            <div className="popupContent" onClick={null}>
-  
-              <div className="popupColumn1">
-                <h1>Image</h1>
-              </div>
-              <div className="popupColumn2">
-                <h1>Title</h1>
-                <h2>Text</h2>
-              </div>
-  
-            </div>
-            <div onClick={()=>this.hidePopup()} className="popupBackground" ></div>
-          </div>
-   */}
-        </Row>
-  
-        <Row className="bodySection2">
-
-          <Col lg={12}>
-            <h1>Who are Leaf.com?</h1>
-              <Row className="sectionContentWrapper m-0">
-                <Col>
-                  <img src={who1} />
-                  <p>Database</p>
-                </Col>
-
-                <Col>
-                  <img src={who2} />
-                  <p>Preservation</p>
-                </Col>
-
-                <Col>
-                  <img src={who3} />
-                  <p>Knowledge</p>
-                </Col>
-              </Row>
-          </Col>
-        </Row>
-
-
-        <Row className="bodySection3">
-
-          <Col lg={12}>
-            <h1>Plant Uses</h1>
-              <Row className="sectionContentWrapper upper m-0 mb-3 ">
-
-                <Col id="forcePaddingRight" md={6} className="d-flex justify-content-center justify-content-md-end">
-                  <img src={placeholderPlant} />
-                </Col>
-
-                <Col md={6} className="d-flex justify-content-center align-items-center">
-                  <div className="p-5">
-                    <h2>Edible</h2>
-                    <p>Plants bring homes to life, and they also bring life to your office. Adding plants into your space also helps to create a relaxing environment.</p>
-                    <p>Plants bring homes to life, and they also bring life to your office. Adding plants into your space also helps to create a relaxing environment.</p>
-                    <Button variant="success">Success</Button>
-                  </div>
-                </Col>
-              
-
-              </Row>
-
-              <Row className="sectionContentWrapper m-0 lower">
-                <Col id="forcePaddingLeft" md={6} className="d-flex justify-content-center justify-content-md-start">
-                  <img src={placeholderPlant} />
-                </Col>
-        
-                <Col md={6} className="d-flex justify-content-center align-items-center">
-                  <div className="p-5">
-                    <h2>Medicinal</h2>
-                    <p>We've gathered some of our most popular plants into sets to suit all types of plant parent, including those who need a bit of extra inspiration.</p>
-                    <p>Find your match</p>
-                    <Button variant="success">Success</Button>
-                  </div>
-                </Col>
-
-              </Row>
-          </Col>
-
-        </Row>
-
-        <Row className="bodySection4">
-
-          <Col lg={12}>
-            <h1>Blog Pages</h1>
-              <Row className="sectionContentWrapper">
-                <Col sm={6} lg={3}>
-                  <div className="pb-3">
-                    <img src={placeholderPlant} />
-                    <p>Blog Title</p>
-                    <p>Blog Copy</p>
-                  </div>
-                </Col>
-
-                <Col sm={6} lg={3}>
-                  <div>
-                    <img src={placeholderPlant} />
-                    <p>Blog Title</p>
-                    <p>Blog Copy</p>
-                  </div>
-                </Col>
-
-                <Col sm={6} lg={3}>
-                  <div>
-                    <img src={placeholderPlant} />
-                    <p>Blog Title</p>
-                    <p>Blog Copy</p>
-                  </div>
-                </Col>
-
-                <Col sm={6} lg={3}>
-                  <div>
-                    <img src={placeholderPlant} />
-                    <p>Blog Title</p>
-                    <p>Blog Copy</p>
-                  </div>
-                </Col>
-              </Row>
-          </Col>
-
-          </Row>
-  
-      </Container>
 
       <Container fluid="lg" className="footer">
   
