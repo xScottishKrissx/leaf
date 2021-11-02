37c7170c2c35afea34881778249a70b328d5d7e4
Chris Dunne <chrisdunne66@gmail.com>
2021-11-02 11:04:26 +0000

    0.23 - Mobile Testing - Plant Gallery layout tweak

--- src/components/body/gallery/galleryModalContent.js
+++ src/components/body/gallery/galleryModalContent.js
@@ -8,7 +8,8 @@ import { BsBrightnessHigh, BsDropletFill } from 'react-icons/bs';
 import { IoMdTimer } from 'react-icons/io'
 import { IoColorPaletteOutline } from 'react-icons/io5'
 import { VscSymbolRuler } from 'react-icons/vsc'
-
+import {GiFamilyTree} from 'react-icons/gi'
+import { MdOutlineScience } from 'react-icons/md'
 export default function GalleryModalContent(props) {
     return (
         <>
@@ -19,12 +20,14 @@ export default function GalleryModalContent(props) {
                     <div className="d-flex justify-content-evenly">
 
                     <div sm={6} className="d-flex flex-column">
+                        <span title="Scientific Name"><MdOutlineScience /> {props.plantInfo.latinName}</span>
                         <span title="Light Levels"><BsBrightnessHigh /> {props.plantInfo.light}</span>
                         <span title="Water Requirements"><BsDropletFill />{props.plantInfo.water}</span>
                         <span title="Colours"><IoColorPaletteOutline />{props.plantInfo.colour}</span>
                     </div>
 
                     <div sm={6} className="d-flex flex-column">
+                        <span title="Family"><GiFamilyTree /> {props.plantInfo.family}</span>
                         <span title="Maximum Height"><VscSymbolRuler />{props.plantInfo.maxHeight}</span>
                         <span title="Maximum time to full height"><IoMdTimer />{props.plantInfo.maxTimeToGrow}</span>
                         <span title="Native To"><BiWorld />{props.plantInfo.nativeTo}</span>             
@@ -39,13 +42,11 @@ export default function GalleryModalContent(props) {
                     <Row>
                         <Col className="text-center">                
                             <h1 id="commonName">{props.plantInfo.commonName}</h1>
-                            <h4 id="latinName">{props.plantInfo.latinName}</h4>
-                            <h5 id="family">{props.plantInfo.family}</h5>
                         </Col>
                     </Row>
 
                     <Row>
-                        <Col>
+                        <Col >
                             <p id="about">{props.plantInfo.about}</p>
                         </Col>
                     </Row>
