ebcf9dbd4e59aa9f266e07ca851ebd4535a53c0c
Chris Dunne <chrisdunne66@gmail.com>
2021-11-02 12:13:17 +0000

    0.24 - Testing Animations

--- src/components/body/bio/bio.js
+++ src/components/body/bio/bio.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import './bio.sass'
@@ -10,12 +10,39 @@ import Modal from '../../modal/modal'
 export const Bio = (props) =>{
   const [bioInfo, setBioInfo] = useState([])
   const [displayPopup, setPopupStatus] = useState(false)
+  const [addClass, setClass] = useState()
 
   function showPopup(e,info){
     if(displayPopup === true)setPopupStatus(false)
     if(displayPopup === false)setPopupStatus(true)
     setBioInfo(info)
   }
+
+
+  useEffect(() => {
+
+    function intersectionCallback(entries) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let targetRow = entry.target.className
+          if(targetRow.includes("bioItemWrapper")){
+             setClass("testAnimate")
+             console.log("Add Class")
+          }
+         
+        } 
+        else {}
+      });
+    }
+    
+    let options = { threshold: [0.25] }
+    let observer = new IntersectionObserver(intersectionCallback, options)
+    let elements = document.querySelectorAll('div')
+    for (let elm of elements)observer.observe(elm)
+    window.addEventListener("scroll", {passive: true});
+
+    return () => window.removeEventListener("scroll", {passive: true});
+  }, [addClass]);
 
 
   // console.log(props.bio)
@@ -27,12 +54,13 @@ export const Bio = (props) =>{
     </Col>
     )
   })
-    
+
+  let test = addClass  
 return(
     <Row className="my-5">
       <Col lg={12}>
         <h1>Who are Leaf.com?</h1>
-        <Row className="bioItemWrapper m-0" > {mapBio} </Row>
+        <Row className={"bioItemWrapper m-0 " + test}  > {mapBio} </Row>
       </Col>
 
       {displayPopup === true ?
